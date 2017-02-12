73a805568253e2661969140e30a9952dfa14f6fa
Aloïs Deniel <alois.deniel@gmail.com>
2017-02-12 19:51:57 +0100

    First bindings and sample.

---
+++ Xam.MaterialTextField/Xam.MaterialTextField.Sample/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿
+using Android.App;
+using Android.Graphics;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+
+namespace Xam.MaterialTextField.Sample
+{
+	[Activity(Label = "MaterialTextField", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Main);
+
+			var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+			SetSupportActionBar(toolbar);
+			toolbar.SetTitleTextColor(Color.White);
+
+			var supportActionBar = SupportActionBar;
+			if (supportActionBar != null)
+			{
+				supportActionBar.SetDisplayShowHomeEnabled(true);
+			}
+		}
+	}
+}
+
